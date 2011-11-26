7f9a4f0457a68df768249334aa0ecb3afb737587
evenwu <evendesign@gmail.com>
2011-11-26 17:55:06 +0800

    更改asset目錄

--- config.rb
+++ config.rb
@@ -60,13 +60,13 @@
 # end
 
 # Change the CSS directory
-# set :css_dir, "alternative_css_directory"
+set :css_dir, "asset/stylesheets"
 
 # Change the JS directory
-# set :js_dir, "alternative_js_directory"
+set :js_dir, "asset/javascripts"
 
 # Change the images directory
-# set :images_dir, "alternative_image_directory"
+set :images_dir, "asset/images"
 
 # Build-specific configuration
 configure :build do
